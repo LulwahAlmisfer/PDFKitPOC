9066a5acd641009830888f0825d81626f58f14d0
Lulwah Almisfer <con-lalmasfer@pif.gov.sa>
2025-01-24 11:01:08 +0300

    Initial Commit

---
+++ PDFKitPOC/PDFKitPOCApp.swift
@@ -0,0 +1,17 @@
+//
+//  PDFKitPOCApp.swift
+//  PDFKitPOC
+//
+//  Created by Lulwah almisfer on 24/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct PDFKitPOCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
